a6e89006048ad753ff625fc872dd2dc92eea7c1b
sjoulbak <remco_meinen@hotmail.com>
2017-05-03 09:07:16 +0200

    Added config tests and using tests
    We should cover everything, because it should
    be possible to know how this dependency works
    by only reading the tests.

--- test/phoenix_inline_svg/helpers_test.exs
+++ test/phoenix_inline_svg/helpers_test.exs
@@ -11,6 +11,18 @@ defmodule PhoenixInlineSvg.HelpersTest do
     {:ok, conn: build_conn()}
   end
 
+  describe "__using__" do
+    # My approach for here was to assert the RuntimeError, but still wip.
+    # test "should raise an error when no opt_app is given" do
+    #   try do
+    #     require PhoenixInlineSvg.Helpers
+    #     PhoenixInlineSvg.Helpers.__using__([])
+    #   rescue
+    #     x in RuntimeError -> IO.inspect x
+    #   end
+    # end
+  end
+
   describe "svg_image/2" do
     test "should show a svg", %{conn: conn} do
       {:safe, svg} = Helpers.svg_image(conn, "icon")
@@ -46,6 +58,33 @@ defmodule PhoenixInlineSvg.HelpersTest do
       assert svg == "<i class='-svgs -notsvg-svg'><svg viewbox='0 0 60 60'>" <>
                     "<text x='0' y='40' font-size='30' font-weight='bold' font-family='monospace'>Err</text></svg>" <>
                     "</i>"
+    end
+  end
+
+  describe "config" do
+    test "should use the default config", %{conn: conn} do
+      # We need another approach for this, because otherwise it is possible to
+      # have random failures.
+      #
+      # env = Application.get_env(:phoenix_inline_svg, :not_found)
+      # Application.delete_env(:phoenix_inline_svg, :not_found)
+      #
+      # {:safe, svg} = Helpers.svg_image(conn, "icon")
+      # assert svg == "<svg viewbox='0 0 60 60'><text x='0' y='40' font-family='monospace'>Test</text></svg>"
+      #
+      # Application.put_env(:phoneix_inline_svg, :not_found, env)
+    end
+
+    test "variabele :dir should have a default value" do
+      assert Application.get_env(:phoenix_inline_svg, :dir) == "test/fixtures"
+    end
+
+    test "variabele :default_collection should have a default value" do
+      assert Application.get_env(:phoenix_inline_svg, :default_collection) == ""
+    end
+
+    test "variabele :not_found should have a default value" do
+      assert Application.get_env(:phoenix_inline_svg, :not_found) == "<svg viewbox='0 0 60 60'><text x='0' y='40' font-size='30' font-weight='bold' font-family='monospace'>Err</text></svg>"
     end
   end
 
