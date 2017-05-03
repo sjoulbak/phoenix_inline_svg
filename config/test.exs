0e1c1bf45a8a937ac385503e4a484e4e0ad5cea9
sjoulbak <remco_meinen@hotmail.com>
2017-05-03 08:39:59 +0200

    Added test config for testing purpose

--- config/test.exs
+++ config/test.exs
@@ -1,8 +1,6 @@
 use Mix.Config
 
 config :phoenix_inline_svg,
-  dir: "priv/static/svg/",
-  default_collection: "generic",
+  dir: "test/fixtures",
+  default_collection: "",
   not_found: "<svg viewbox='0 0 60 60'><text x='0' y='40' font-size='30' font-weight='bold' font-family='monospace'>Err</text></svg>"
-
-import_config "#{Mix.env}.exs"
